bc3e1f899fb9983dac7f5f12060f09a0b2ba4fba
Sean Rankine <sean.rankine@me.com>
2023-10-10 21:31:11 +0100

    Add icon mime type
    This adds support for "image/vnd.microsoft.icon" content type icons.

--- internal/mime/mimetypes.go
+++ internal/mime/mimetypes.go
@@ -5,23 +5,25 @@ import (
 	"mime"
 )
 
-var additionalMimeTypes = map[string]string{
-	".atom": "application/atom+xml",
-	".csv":  "text/csv",
-	".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-	".ico":  "image/x-icon",
-	".ics":  "text/calendar",
-	".odp":  "application/vnd.oasis.opendocument.presentation",
-	".ods":  "application/vnd.oasis.opendocument.spreadsheet",
-	".odt":  "application/vnd.oasis.opendocument.text",
-	".xls":  "application/vnd.ms-excel",
-	".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+var additionalMimeTypes = map[string][]string{
+	".atom": {"application/atom+xml"},
+	".csv":  {"text/csv"},
+	".docx": {"application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+	".ico":  {"image/vnd.microsoft.icon", "image/x-icon"},
+	".ics":  {"text/calendar"},
+	".odp":  {"application/vnd.oasis.opendocument.presentation"},
+	".ods":  {"application/vnd.oasis.opendocument.spreadsheet"},
+	".odt":  {"application/vnd.oasis.opendocument.text"},
+	".xls":  {"application/vnd.ms-excel"},
+	".xlsx": {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
 }
 
 func LoadAdditionalMimeTypes() error {
-	for ext, typ := range additionalMimeTypes {
-		if err := mime.AddExtensionType(ext, typ); err != nil {
-			return fmt.Errorf("error adding mime type %s with extension %s: %w", typ, ext, err)
+	for ext, types := range additionalMimeTypes {
+		for _, typ := range types {
+			if err := mime.AddExtensionType(ext, typ); err != nil {
+				return fmt.Errorf("error adding mime type %s with extension %s: %w", typ, ext, err)
+			}
 		}
 	}
 	return nil
