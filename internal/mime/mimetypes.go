675217d3fa7051eead8e58e47aadb7f803410e9e
Sean Rankine <sean.rankine@me.com>
2023-10-11 07:13:45 +0100

    Add XML as supported content type
    This is used by our sitemaps.

--- internal/mime/mimetypes.go
+++ internal/mime/mimetypes.go
@@ -16,6 +16,7 @@ var additionalMimeTypes = map[string][]string{
 	".odt":  {"application/vnd.oasis.opendocument.text"},
 	".xls":  {"application/vnd.ms-excel"},
 	".xlsx": {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+	".xml":  {"application/xml"},
 }
 
 func LoadAdditionalMimeTypes() error {
