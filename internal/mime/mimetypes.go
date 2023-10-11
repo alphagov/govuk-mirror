2f768545f6f659e98aeed894091dc47c5733171b
Sean Rankine <sean.rankine@me.com>
2023-10-11 08:56:17 +0100

    Add application/javascript mime type
    Our applications still use the "application/javascript" content type for
    .js files (which is considered obsolete, replaced by text/javascript).

--- internal/mime/mimetypes.go
+++ internal/mime/mimetypes.go
@@ -17,6 +17,7 @@ var additionalMimeTypes = map[string][]string{
 	".xls":  {"application/vnd.ms-excel"},
 	".xlsx": {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
 	".xml":  {"application/xml"},
+	".js":   {"application/javascript"},
 }
 
 func LoadAdditionalMimeTypes() error {
