5819b261fb92bfd6f430cfa961fb7195d410f1b7
Sean Rankine <sean.rankine@me.com>
2023-10-12 11:15:58 +0100

    Add support for font content types
    This adds media types for woff and woff2

--- internal/mime/mimetypes.go
+++ internal/mime/mimetypes.go
@@ -6,18 +6,20 @@ import (
 )
 
 var additionalMimeTypes = map[string][]string{
-	".atom": {"application/atom+xml"},
-	".csv":  {"text/csv"},
-	".docx": {"application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
-	".ico":  {"image/vnd.microsoft.icon", "image/x-icon"},
-	".ics":  {"text/calendar"},
-	".odp":  {"application/vnd.oasis.opendocument.presentation"},
-	".ods":  {"application/vnd.oasis.opendocument.spreadsheet"},
-	".odt":  {"application/vnd.oasis.opendocument.text"},
-	".xls":  {"application/vnd.ms-excel"},
-	".xlsx": {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
-	".xml":  {"application/xml"},
-	".js":   {"application/javascript"},
+	".atom":  {"application/atom+xml"},
+	".csv":   {"text/csv"},
+	".docx":  {"application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+	".ico":   {"image/vnd.microsoft.icon", "image/x-icon"},
+	".ics":   {"text/calendar"},
+	".odp":   {"application/vnd.oasis.opendocument.presentation"},
+	".ods":   {"application/vnd.oasis.opendocument.spreadsheet"},
+	".odt":   {"application/vnd.oasis.opendocument.text"},
+	".xls":   {"application/vnd.ms-excel"},
+	".xlsx":  {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+	".xml":   {"application/xml"},
+	".js":    {"application/javascript"},
+	".woff":  {"font/woff"},
+	".woff2": {"font/woff2"},
 }
 
 func LoadAdditionalMimeTypes() error {
