572edf9e6005fa36359212ccc8fded125f6820bd
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2025-06-04 17:01:48 +0100

    🐛 Change casing in the error output

--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -69,7 +69,7 @@ func TestGenerateFilePathTableDriven(t *testing.T) {
 		{"https://example.com/foo.csv/preview", "text/html", "example.com/foo.csv/preview.html", nil},
 		{"https://example.com/style.css", "text/css", "example.com/style.css", nil},
 		{"https://example.com/foo", "text/blah", "", errors.New("Error determining content type")},
-		{"https://example.com/foo", "", "", errors.New("Error determining content type")},
+		{"https://example.com/foo", "", "", errors.New("error determining content type")},
 		{"https://example.com/foo.cy", "text/html", "example.com/foo.cy.html", nil},
 		{"https://example.com/foo.html", "text/html", "example.com/foo.html", nil},
 		{"https://example.com/foo//bar", "text/html", "example.com/foo/bar.html", nil},
