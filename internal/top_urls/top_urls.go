d9cc09fd2b173767b566129044daacf15cf0ea4f
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-11-13 12:57:45 +0000

    Add a TopUrls type, a top urls client interface, and a mock for it

---
+++ internal/top_urls/top_urls.go
@@ -0,0 +1,70 @@
+package top_urls
+
+import (
+	"cmp"
+	"fmt"
+	"math/rand"
+	"net/url"
+	"slices"
+)
+
+type TopUrls struct {
+	topUnsampledUrls     []UrlHitCount
+	remainingSampledUrls []UrlHitCount
+}
+
+type UrlHitCount struct {
+	viewedUrl *url.URL
+	viewCount int64
+}
+
+func NewTopUrls(urls []UrlHitCount, numberUnsampled int, numberToSample int, random *rand.Rand) (*TopUrls, error) {
+	totalUrls := len(urls)
+	if totalUrls < numberUnsampled {
+		return nil, fmt.Errorf("requested %d unsampled, but there are only %d topUrls", numberUnsampled, totalUrls)
+	}
+
+	numUrlsAvailableToSample := totalUrls - numberUnsampled
+	if numUrlsAvailableToSample < numberToSample {
+		return nil, fmt.Errorf(
+			"requested %d sampled, but there are only %d urls available to sample once the top %d are removed",
+			numberToSample,
+			numUrlsAvailableToSample,
+			numberUnsampled,
+		)
+	}
+
+	copiedUrls := make([]UrlHitCount, totalUrls)
+
+	numCopiedUrls := copy(copiedUrls, urls)
+	if numCopiedUrls != totalUrls {
+		return nil, fmt.Errorf("expected there to be %d urls to sample from, but there was only %d", totalUrls, numCopiedUrls)
+	}
+
+	slices.SortFunc(copiedUrls, func(a, b UrlHitCount) int {
+		return cmp.Compare(b.viewCount, a.viewCount)
+	})
+
+	sampledUrls, err := sampleRemainingUrls(copiedUrls[numberUnsampled:totalUrls], numberToSample, random)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TopUrls{
+		topUnsampledUrls:     copiedUrls[0:numberUnsampled],
+		remainingSampledUrls: sampledUrls,
+	}, nil
+}
+
+func sampleRemainingUrls(urlsToSample []UrlHitCount, numberToSample int, random *rand.Rand) ([]UrlHitCount, error) {
+	maxPossibleSamples := len(urlsToSample)
+	if numberToSample > maxPossibleSamples {
+		return nil, fmt.Errorf("cannot sample %d elements since there are only %d to choose from", numberToSample, maxPossibleSamples)
+	}
+
+	random.Shuffle(len(urlsToSample), func(i, j int) {
+		urlsToSample[i], urlsToSample[j] = urlsToSample[j], urlsToSample[i]
+	})
+
+	return urlsToSample[0:numberToSample], nil
+}
---
+++ internal/top_urls/top_urls_test.go
@@ -0,0 +1,108 @@
+package top_urls
+
+import (
+	"fmt"
+	"math/rand"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var urls = [10]string{
+	"/Lorem",
+	"/ipsum",
+	"/dolor",
+	"/sit",
+	"/amet",
+	"/consectetur",
+	"/adipiscing",
+	"/elit",
+	"/sed",
+	"/do",
+}
+
+var counts = [10]int64{
+	2_173,
+	1_928,
+	0,
+	4_619,
+	5,
+	10,
+	3,
+	10_000,
+	500,
+	7_782,
+}
+
+var urlHitCounts = make([]UrlHitCount, len(urls))
+var random = rand.New(rand.NewSource(99))
+
+func TestMain(m *testing.M) {
+	for i, u := range urls {
+		parsedUrl, err := url.Parse(u)
+		if err != nil {
+			panic(fmt.Sprintf("Test setup url %s couldn't be parsed", u))
+		}
+
+		urlHitCounts[i] = UrlHitCount{
+			viewedUrl: parsedUrl,
+			viewCount: counts[i],
+		}
+	}
+
+	m.Run()
+}
+
+func TestNewTopUrlsWithTooManyUnsampledRequested(t *testing.T) {
+	topUrls, err := NewTopUrls(urlHitCounts, 20, 5, random)
+	assert.Nil(t, topUrls)
+	assert.Error(t, err)
+}
+
+func TestNewTopUrlsWithTooManySampledRequested(t *testing.T) {
+	topUrls, err := NewTopUrls(urlHitCounts, 5, 6, random)
+	assert.Nil(t, topUrls)
+	assert.Error(t, err)
+}
+
+func TestNewTopUrlsWithTop3And2Sampled(t *testing.T) {
+	expectedUnsampledUrls := []UrlHitCount{
+		urlHitCounts[7],
+		urlHitCounts[9],
+		urlHitCounts[3],
+	}
+	expectedSampledUrls := []UrlHitCount{
+		urlHitCounts[0],
+		urlHitCounts[4],
+	}
+
+	topUrls, err := NewTopUrls(urlHitCounts, 3, 2, random)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUnsampledUrls, topUrls.topUnsampledUrls)
+	assert.Equal(t, expectedSampledUrls, topUrls.remainingSampledUrls)
+}
+
+func TestNewTopUrlsWithTop7And3Sampled(t *testing.T) {
+	expectedUnsampledUrls := []UrlHitCount{
+		urlHitCounts[7],
+		urlHitCounts[9],
+		urlHitCounts[3],
+		urlHitCounts[0],
+		urlHitCounts[1],
+		urlHitCounts[8],
+		urlHitCounts[5],
+	}
+	expectedSampledUrls := []UrlHitCount{
+		urlHitCounts[6],
+		urlHitCounts[2],
+		urlHitCounts[4],
+	}
+
+	topUrls, err := NewTopUrls(urlHitCounts, 7, 3, random)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUnsampledUrls, topUrls.topUnsampledUrls)
+	assert.Equal(t, expectedSampledUrls, topUrls.remainingSampledUrls)
+}
